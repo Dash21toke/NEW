064daafff8cbb2af7a03456676a3d19b2a015505
Dash21toke <darshanatoke21@gmail.com>
2022-03-25 15:40:20 +0530

    message

---
+++ main.py
@@ -0,0 +1,118 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mult(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+if'__name__' == '__main__':
+ num1 = int(input("Please Enter the 1st no:"))
+ num2 = int(input("please enter the 2nd number:"))
+
+ print(add(num1,num2))
+ print(sub(num1,num2))
+ print(mult(num1,num2))
+ print(div(num1,num2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
